d4437c56cc97c84ca824280d933cf051f86c2754
EfeAdzhar <efeadzhar@gmail.com>
2022-05-22 12:23:04 +0200

    Made one screen for 2 cells. Finished watching UIKit tutorial and now I will try to implement it in my new project

--- Efe's Quiz/QuizMenu/QuizList.swift
+++ Efe's Quiz/QuizMenu/QuizList.swift
@@ -46,7 +46,7 @@ var imageArray = ["political_orientation",
         self.tableView.layer.addSublayer(layer)
     }
     
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Quiz", for: indexPath)
         cell.imageView?.image = UIImage(named: imageArray[indexPath.row])
